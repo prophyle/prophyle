81d054a04599b44710560ca879b98872b898b793
Karel Břinda <karel.brinda@gmail.com>
2016-02-07 21:48:32 +0100

    CMake external project for GATB finally works.
    Former-commit-id: befc9fecf65d6bcf5f7402f7c1842a2846692228

--- proc/contrib/gatbcore.cmake
+++ proc/contrib/gatbcore.cmake
@@ -1,4 +1,4 @@
-set(gatbcore_PREFIX ${CMAKE_BINARY_DIR}/contrib/gatbcore-prefix/src/gatbcore/gatb-core)
+set(gatbcore_PREFIX ${CMAKE_BINARY_DIR}/contrib/gatbcore-prefix)
 set(gatbcore_INSTALL ${CMAKE_BINARY_DIR}/contrib/gatbcore-install)
 
 ExternalProject_Add(gatbcore
@@ -6,10 +6,11 @@ ExternalProject_Add(gatbcore
 	GIT_REPOSITORY "https://github.com/GATB/gatb-core.git"
 	#GIT_TAG "fix-signed-one-bit-bitfield"
 	UPDATE_COMMAND ""
-	BUILD_IN_SOURCE 1
+	BUILD_IN_SOURCE 0
 	INSTALL_DIR ${gatbcore_INSTALL}
 	CMAKE_ARGS -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
 	-DCMAKE_INSTALL_PREFIX=${gatbcore_INSTALL}
+	SOURCE_DIR "${gatbcore_PREFIX}/src/gatb-core"
 	#-DAMD64=ON
 	#CMAKE_ARGS "${gtest_cmake_args}" "-DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}"
 	LOG_DOWNLOAD ON
@@ -19,6 +20,8 @@ ExternalProject_Add(gatbcore
 
 
 include_directories(${gatbcore_INSTALL}/include)
-#add_dependencies(google_test)
+#add_dependencies(gatbcore)
+set(gatbcore_LIB ${gatbcore_INSTALL}/lib/libgatbcore.a)
+
 #set(googletest_LIB ${gatbcore_INSTALL}/lib/libgtest.a ${googletest_INSTALL}/lib/libgtest_main.a)
 
