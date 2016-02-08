e97438b7cee98ad77d0d2e65b4e4d098181f3022
Karel Břinda <karel.brinda@gmail.com>
2016-02-08 20:04:04 +0100

    First working assembly.
    Former-commit-id: 3232951af5f371ec09a37a11c4fb245d2041649b

--- proc/main.cpp
+++ proc/main.cpp
@@ -2,7 +2,10 @@
 #include <unordered_map>
 #include <boost/unordered_set.hpp>
 #include <unordered_set>
+#include <vector>
 #include "kseq.h"
+
+#include <cstdio>
 
 #include <gatb/tools/math/Integer.hpp>
 
@@ -43,11 +46,9 @@ struct contig_t{
 	char *r_ext;
 	char *l_ext;
 
-	contig_t(const char *base_kmer){
-		k=strlen(base_kmer);
+	contig_t(uint32_t k){
+		this->k=k;
 		seq_buffer=new char[k+2*max_contig_length+1]();
-
-		reinit(base_kmer);
 	}
 
 	int32_t reinit(const char *base_kmer){
@@ -59,24 +60,27 @@ struct contig_t{
 		for(int32_t i=0;i<k;i++){
 			r_extend(base_kmer[i]);
 		}
+		return 0;
 	}
 
 	int32_t r_extend(char c){
 		*r_ext=c;
 		++r_ext;
 		*r_ext='\0';
+		return 0;
 	}
 
 	int32_t l_extend(char c){
 		--l_ext;
 		*l_ext=complement(c);
+		return 0;
 	}
 
 	~contig_t(){
 		delete[] seq_buffer;
 	}
 
-	int32_t print_to_fasta(FILE* fasta_file, char* contig_name, char *comment){
+	int32_t print_to_fasta(FILE* fasta_file, char* contig_name, char *comment=nullptr) const {
 		if (comment==nullptr){
 			fprintf(fasta_file,">%s\n",contig_name);
 		} else {
@@ -93,7 +97,7 @@ struct contig_t{
 			fprintf(fasta_file, "%s\n", print_buffer);
 		}		
 
-		delete[] seq_buffer;
+		return 0;
 	}
 };
 
@@ -101,8 +105,9 @@ struct contig_t{
 /*
 	TODO: test if kmer is correct
 */
-int ordered_kmers_from_fasta(const string &fasta_fn, ordered_set_t &ordered_set, int32_t k){
-	ordered_set.clear();
+template <typename T>
+int kmers_from_fasta(const string &fasta_fn, T &set, int32_t k){
+	set.clear();
 
  	gzFile fp;
     kseq_t *seq;
@@ -117,17 +122,19 @@ int ordered_kmers_from_fasta(const string &fasta_fn, ordered_set_t &ordered_set,
 
 
 	for(int32_t seqid=0;(l = kseq_read(seq)) >= 0;seqid++) {
- 		//seq->seq.s;
-	    Data data (seq->seq.s);
-	
+		cout << "start" << endl;
+	    Data data (seq->seq.s);	
+		cout << "data ok" << endl;
 	    Kmer<>::ModelCanonical::Iterator it (model);
 	    it.setData (data);
 
+		cout << "starting iterator" << endl;
 	    for (it.first(); !it.isDone(); it.next())
 	    {
-	    	std::cout << it->value() << " " << model.toString( it->value() ) << std::endl;
+	    	set.insert(it->value().getVal());
+	    	//std::cout << it->value() << " " << model.toString( it->value() ) << std::endl;
 	    }
-
+		cout << "iterator finished" << endl;
     }
 
     kseq_destroy(seq);
@@ -136,7 +143,7 @@ int ordered_kmers_from_fasta(const string &fasta_fn, ordered_set_t &ordered_set,
 	return 0;
 }
 
-int unsort_kmers(const ordered_set_t &ordered_set, unordered_set_t &unordered_set){
+int ordered_to_unordered(const ordered_set_t &ordered_set, unordered_set_t &unordered_set){
 	unordered_set.clear();
 
     for(nkmer_t const &element: ordered_set){
@@ -145,8 +152,74 @@ int unsort_kmers(const ordered_set_t &ordered_set, unordered_set_t &unordered_se
 	return 0;
 }
 
-int ordered_kmers_to_fasta(const string &fasta_fn, ordered_set_t &ordered_set, int32_t k){
+template <typename T>
+int assemble(const string &fasta_fn, T &set, int32_t k){
+    Kmer<>::ModelCanonical model (k);
+
+
+    cout << "assembling, size: " << set.size() << endl;
+
+	FILE *file=fopen(fasta_fn.c_str(),"w+");
+
+	char kmer_str[k+1];
+
+	contig_t contig(k);
+
+	const std::vector<char> nucls = {'A','C','G','T'};
+
+	int i=0;
+	while(set.size()>0){
+		i++;
+		//printf("writing contig %d\n",i);
+		const nkmer_t central_kmer=*(set.begin());
+		set.erase(central_kmer);
+
+		std::string central_kmer_string=model.toString(central_kmer);
+		contig.reinit(central_kmer_string.c_str());
+
+		strncpy(kmer_str,central_kmer_string.c_str(),k);
+		kmer_str[k]='\0';
+
+		//printf("central k-mer: %s\n",central_kmer_string.c_str());
+
+		bool extending = true;
+		while (extending){
+			
+			for(int32_t i=0;i<k;i++){
+				kmer_str[i]=kmer_str[i+1];
+			}
+			kmer_str[k]='\0';
+
+
+			extending=false;
+			for(const char &c : nucls){
+				kmer_str[k-1]=c;
+
+
+				nkmer_t nkmer = model.codeSeed (
+ 					kmer_str , Data::ASCII
+				).value().getVal();
+
+
+				if(set.count( nkmer )){
+					contig.r_extend(c);
+					extending=true;
+					set.erase(nkmer);
+					break;
+				}
+
+			}
+		}
+
+		contig.print_to_fasta(file,"conting xx");
+	}
+
+	fclose(file);
+
+    cout << "...finished " << endl;
+
 	return 0;
+
 }
 
 
@@ -162,8 +235,63 @@ int ordered_kmers_to_fasta(const string &fasta_fn, ordered_set_t &ordered_set, i
 /********************************************************************************/
 int main (int argc, char* argv[])
 {
-	ordered_set_t a;
-	ordered_kmers_from_fasta("test.fa", a, 5);
+	std::string fasta_in1="test1.fa";
+	std::string fasta_in2="test2.fa";
+	std::string fasta_out1="out1.fa";
+	std::string fasta_out2="out2.fa";
+	std::string fasta_out3="out3.fa";
+
+	//const int32_t init_size=2000000;
+
+	ordered_set_t os_in1;
+	ordered_set_t os_in2;
+	ordered_set_t os_out1;
+	ordered_set_t os_out2;
+	ordered_set_t os_out3;
+
+	unordered_set_t us_in1;
+	unordered_set_t us_in2;
+	unordered_set_t us_out1;
+	unordered_set_t us_out2;
+	unordered_set_t us_out3;
+
+	cout << "ordered in1" << endl;
+	kmers_from_fasta(fasta_in1, os_in1, k);
+	cout << "size " << os_in1.size() << endl;
+	cout << "ordered in2" << endl;
+	kmers_from_fasta(fasta_in2, os_in2, k);
+
+	//cout << "unordered in1" << endl;
+	//std::copy(us_in1.begin(), us_in1.end(), std::inserter(os_in1,os_in1.begin()));
+	//cout << "unordered in2" << endl;
+	//std::copy(us_in2.begin(), us_in2.end(), std::inserter(os_in2,os_in2.begin()));
+
+
+	cout << "ordered out3" << endl;
+	std::set_intersection(
+		os_in1.begin(), os_in1.end(),
+		os_in2.begin(), os_in2.end(),
+    	std::inserter(os_out3, os_out3.end())
+    );
+	//cout << "unordered out3" << endl;
+	//std::copy(os_out3.begin(), os_out3.end(), std::inserter(us_out3,us_out3.begin()));
+
+	cout << "ordered out1" << endl;
+	std::set_symmetric_difference(
+		os_in1.begin(), os_in1.end(),
+		os_out3.begin(), os_out3.end(),
+    	std::inserter(os_out1, os_out1.end())
+    );
+
+	cout << "ordered out2" << endl;
+	std::set_symmetric_difference(
+		os_in2.begin(), os_in2.end(),
+		os_out3.begin(), os_out3.end(),
+    	std::inserter(os_out2, os_out2.end())
+    );
+
+	assemble("out.fa",os_in1,k);
+
 	return 0;
 
 
