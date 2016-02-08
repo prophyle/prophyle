09a0d56e522e760e3e31870068cb4e517cdf9979
Karel Břinda <karel.brinda@gmail.com>
2016-02-08 18:14:42 +0100

    New updates.
    Former-commit-id: 8a4f46f0c9dd10a764a94c372bd0a778fc6857d6

--- proc/main.cpp
+++ proc/main.cpp
@@ -2,8 +2,154 @@
 #include <unordered_map>
 #include <boost/unordered_set.hpp>
 #include <unordered_set>
+#include "kseq.h"
 
 #include <gatb/tools/math/Integer.hpp>
+
+const int32_t k=20;
+const int32_t fasta_line_length=60;
+const int32_t max_contig_length=10000000;
+
+KSEQ_INIT(gzFile, gzread);
+
+
+typedef uint64_t nkmer_t;
+typedef std::set<nkmer_t> ordered_set_t;
+typedef std::unordered_set<nkmer_t> unordered_set_t;
+
+char complement(char x){
+	switch(x){
+		case 'A':
+		case 'a':
+			return 'T';
+		case 'C':
+		case 'c':
+			return 'G';
+		case 'G':
+		case 'g':
+			return 'C';
+		case 'T':
+		case 't':
+			return 'A';
+		default:
+			return 'N';
+	}
+}
+
+struct contig_t{
+	int32_t k;
+
+	char *seq_buffer;
+	char *r_ext;
+	char *l_ext;
+
+	contig_t(const char *base_kmer){
+		k=strlen(base_kmer);
+		seq_buffer=new char[k+2*max_contig_length+1]();
+
+		reinit(base_kmer);
+	}
+
+	int32_t reinit(const char *base_kmer){
+		assert(strlen(base_kmer)==k);
+
+		l_ext = r_ext = &seq_buffer[max_contig_length];
+		*r_ext='\0';
+
+		for(int32_t i=0;i<k;i++){
+			r_extend(base_kmer[i]);
+		}
+	}
+
+	int32_t r_extend(char c){
+		*r_ext=c;
+		++r_ext;
+		*r_ext='\0';
+	}
+
+	int32_t l_extend(char c){
+		--l_ext;
+		*l_ext=complement(c);
+	}
+
+	~contig_t(){
+		delete[] seq_buffer;
+	}
+
+	int32_t print_to_fasta(FILE* fasta_file, char* contig_name, char *comment){
+		if (comment==nullptr){
+			fprintf(fasta_file,">%s\n",contig_name);
+		} else {
+			fprintf(fasta_file,">%s %s\n",contig_name,comment);
+		}
+
+		int32_t buffer_len=r_ext-l_ext;
+		char print_buffer[fasta_line_length+1]={0};
+
+		int32_t j=0;
+
+		for (char *p=l_ext;p<r_ext;p+=fasta_line_length){
+			strncpy(print_buffer,p,fasta_line_length);
+			fprintf(fasta_file, "%s\n", print_buffer);
+		}		
+
+		delete[] seq_buffer;
+	}
+};
+
+
+/*
+	TODO: test if kmer is correct
+*/
+int ordered_kmers_from_fasta(const string &fasta_fn, ordered_set_t &ordered_set, int32_t k){
+	ordered_set.clear();
+
+ 	gzFile fp;
+    kseq_t *seq;
+    int64_t l;
+
+    fp = gzopen(fasta_fn.c_str(), "r");
+    seq = kseq_init(fp);
+
+    char buffer[100]={};
+
+    Kmer<>::ModelCanonical model (k);
+
+
+	for(int32_t seqid=0;(l = kseq_read(seq)) >= 0;seqid++) {
+ 		//seq->seq.s;
+	    Data data (seq->seq.s);
+	
+	    Kmer<>::ModelCanonical::Iterator it (model);
+	    it.setData (data);
+
+	    for (it.first(); !it.isDone(); it.next())
+	    {
+	    	std::cout << it->value() << " " << model.toString( it->value() ) << std::endl;
+	    }
+
+    }
+
+    kseq_destroy(seq);
+    gzclose(fp);
+
+	return 0;
+}
+
+int unsort_kmers(const ordered_set_t &ordered_set, unordered_set_t &unordered_set){
+	unordered_set.clear();
+
+    for(nkmer_t const &element: ordered_set){
+     	unordered_set.insert(element);
+    }
+	return 0;
+}
+
+int ordered_kmers_to_fasta(const string &fasta_fn, ordered_set_t &ordered_set, int32_t k){
+	return 0;
+}
+
+
 
 //using boost::unordered_set;
 
@@ -16,6 +162,11 @@
 /********************************************************************************/
 int main (int argc, char* argv[])
 {
+	ordered_set_t a;
+	ordered_kmers_from_fasta("test.fa", a, 5);
+	return 0;
+
+
     // We define a sequence of nucleotides
     //const char* seq = "CATTGATAGTGGATGGT";
     const char* seq = "TTTTTTTCCACFGTCCCCCCCCCC";
@@ -57,6 +208,8 @@ int main (int argc, char* argv[])
     for(auto const &element: s){
      	cout << element  << endl;
     }
+
+
 
 
 }
