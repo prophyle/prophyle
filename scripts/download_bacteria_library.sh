53286accba4fb5888ad655eec3e50d910aa5b7b6
Karel Břinda <karel.brinda@gmail.com>
2016-02-17 19:37:36 +0100

    Fix script for download.

--- scripts/download_bacteria_library.sh
+++ scripts/download_bacteria_library.sh
@@ -3,4 +3,5 @@ cd "$(dirname "$0")"
 
 wget http://monge.univ-mlv.fr/~brinda/library/library__full_bacteria.tar.gz
 tar xvf library__full_bacteria.tar.gz
-mv -f library_full_bacteria/Bacteria ../libraries
+mkdir -p ../library
+mv -f library_full_bacteria/Bacteria ../library
