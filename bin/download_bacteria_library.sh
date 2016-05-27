cc187295799d85c792e543062e05833fbe5b721e
Karel Břinda <karel.brinda@gmail.com>
2016-05-27 17:22:09 +0200

    Finalization of Makefiles
    Former-commit-id: 930a755cc48019c00d056fa7c058c1fa9c53779a

--- bin/download_bacteria_library.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-cd "$(dirname "$0")"
-
-wget http://monge.univ-mlv.fr/~brinda/library/library__full_bacteria.tar.gz
-tar xvf library__full_bacteria.tar.gz
-mkdir -p ../library
-mv -f library__full_bacteria/Bacteria ../library
