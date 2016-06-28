b414966949a52c7e99f9d9315e580ff9ff0374b5
Simone Pignotti <pignottisimone@gmail.com>
2016-06-28 20:48:58 +0200

    Unit test for exk (BUG revealed)
    * Generate all the possible 4-mers
    * Run exk on a small fasta
    * Results to be compared manually

---
+++ tests/exk_exhaustive/kmer_gen.py
@@ -0,0 +1,19 @@
+import sys, os, argparse
+from itertools import product, repeat
+
+parser = argparse.ArgumentParser(description =
+			'Generate all possible k-mers with given lenght')
+
+parser.add_argument('k', type=int, help = 'k-mers lenght')
+parser.add_argument('-o', default = 'test_kmers.fq',
+					help = 'output file (default: test_kmers.fq)')
+
+args = parser.parse_args()
+
+with open(args.o, 'w') as output:
+	i = 1
+	for kmer in product('ACGT', repeat=args.k):
+		output.write('@seq'+str(i)+'\n'+
+					''.join(kmer)+'\n'+
+					'+\n'+''.join(repeat('~',args.k))+'\n')
+		i += 1
