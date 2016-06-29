3d520c5e53c36ead6dff93bb59b1119351290558
Simone Pignotti <pignottisimone@gmail.com>
2016-06-29 18:46:12 +0200

    Small fix

--- tests/exk_exhaustive/test_exk.sh
+++ tests/exk_exhaustive/test_exk.sh
@@ -7,6 +7,6 @@ set -o pipefail
 cd "$(dirname "$0")"
 
 ./kmer_gen.py 4
-../../bin/exk_match_1step.py -k 4 bwa_index/test_seq.fa ./test_kmers.fq > match.txt
+../../bin/exk_match_1step.py -k 4 bwa_index/test_seq.fa ./gen_kmers.fq > match.txt
 cat match.txt | grep -B 1 '^[1-9]' > results.txt
 ./test_equal.py
