6c390e1e23e433246809beb1647cc4e6d7a87a41
Simone Pignotti <pignottisimone@gmail.com>
2016-06-29 18:40:08 +0200

    Add script for automatic check of results and remove useless files

--- tests/exk_exhaustive/kmer_gen.py
+++ tests/exk_exhaustive/kmer_gen.py
@@ -1,3 +1,5 @@
+#! /usr/bin/env python3
+
 import sys, os, argparse
 from itertools import product, repeat
 
@@ -5,8 +7,8 @@ parser = argparse.ArgumentParser(description =
 			'Generate all possible k-mers with given lenght')
 
 parser.add_argument('k', type=int, help = 'k-mers lenght')
-parser.add_argument('-o', default = 'test_kmers.fq',
-					help = 'output file (default: test_kmers.fq)')
+parser.add_argument('-o', default = 'gen_kmers.fq',
+					help = 'output file (default: gen_kmers.fq)')
 
 args = parser.parse_args()
 
--- tests/exk_exhaustive/test_exk.sh
+++ tests/exk_exhaustive/test_exk.sh
@@ -6,16 +6,7 @@ set -o pipefail
 
 cd "$(dirname "$0")"
 
-../../bin/exk index -k 4  bwa_index/test_seq.fa
-
-../../bin/exk match -k 4 -v -u bwa_index/test_seq.fa ./test_kmers.fq > match_rolling.txt
-../../bin/exk match -k 4 -v bwa_index/test_seq.fa ./test_kmers.fq > match_restarted.txt
-
-diff match_rolling.txt match_restarted.txt
-
-../../bin/exk match -s -k 4 -v -u bwa_index/test_seq.fa ./test_kmers.fq > match_rolling_skip.txt
-../../bin/exk match -s -k 4 -v bwa_index/test_seq.fa ./test_kmers.fq > match_restarted_skip.txt
-
-diff match_rolling_skip.txt match_restarted_skip.txt
-
-# cat match_restarted.txt | grep -B 1 '^[1-9]'
+./kmer_gen.py 4
+../../bin/exk_match_1step.py -k 4 bwa_index/test_seq.fa ./test_kmers.fq > match.txt
+cat match.txt | grep -B 1 '^[1-9]' > results.txt
+./test_equal.py
