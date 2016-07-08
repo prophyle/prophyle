1c763b867733899d9431e4930e19c912bb8d3a06
Simone Pignotti <pignottisimone@gmail.com>
2016-07-08 10:40:11 +0200

    Translate index

--- src/kraken-db/converter.cpp
+++ src/kraken-db/converter.cpp
@@ -18,42 +18,51 @@ using namespace std;
 using namespace kraken;
 
 static const char * INDEX = "database.idx";
+static const char * DB = "database.kdb";
+static const uint64_t MASK = uint64_t(3) << 62;
 
-static void check (int test, const char * message, ...)
-{
-    if (test) {
-        va_list args;
-        va_start (args, message);
-        vfprintf (stderr, message, args);
-        va_end (args);
-        fprintf (stderr, "\n");
-        exit (EXIT_FAILURE);
-    }
-}
-
-size_t getFilesize(const char* filename) {
-	struct stat st;
-	if(stat(filename, &st) != 0) {
-		return 0;
+char* kmerInterpreter(uint64_t kmer, uint8_t k) {
+	char* res = new char[k];
+	kmer <<= sizeof(kmer) * 8 - (k * 2);
+	for(int i=0; i<k; i++) {
+		switch ((kmer & MASK) >> 62) {
+			case 0:
+				res[i] = 'A';
+				break;
+			case 1:
+				res[i] = 'C';
+				break;
+			case 2:
+				res[i] = 'G';
+				break;
+			case 3:
+				res[i] = 'T';
+				break;
+		}
+		kmer <<= 2;
 	}
-	return st.st_size;   
-}
-
-void openFileRO(const char* filename, const char **fptr) {
-	int fd = open(filename, O_RDONLY);
-	check (fd < 0, "open %s failed: %s", filename, strerror (errno));
-	size_t size = getFilesize(filename);
-	check (size == 0, "stat %s failed: %s", filename, strerror (errno));
-	*fptr = (char*) mmap(0, size, PROT_READ, MAP_PRIVATE, fd, 0);
-	check (*fptr == MAP_FAILED, "mmap %s failed: %s",
-           filename, strerror (errno));
-	//if (fptr == MAP_FAILED)
-	//	err(EX_OSERR, "unable to mmap %s", filename);
+	return res;
 }
 
 int main() {
-	const char* idx_file;
-	openFileRO(INDEX, &idx_file);
-	char* kraken_idx_file = (char *) idx_file;
-	KrakenDBIndex idx(kraken_idx_file);
+	ofstream out_file;
+	out_file.open("out");
+	QuickFile idx_file(INDEX);
+	KrakenDBIndex idx(idx_file.ptr());
+	idx_file.load_file();
+	QuickFile db_file(DB);
+	KrakenDB db(db_file.ptr());
+	db.set_index(&idx);
+	// uint64_t key_len = db.get_key_len();
+	// uint64_t val_len = db.get_val_len();
+	// uint64_t key_ct = db.get_key_ct();
+	// uint64_t skip_len = db.header_size();
+	uint64_t* bins = idx.get_array();
+	uint64_t nt = idx.indexed_nt();
+	uint64_t sz = sizeof(nt);
+	for(uint64_t i = 0; i < 1 + (1ull << (nt * 2)); i++) {
+		out_file << bins[i] << endl;
+	}
+	
+	return 0;
 }
