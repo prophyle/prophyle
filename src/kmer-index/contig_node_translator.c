2d22df1a736748fcc79196b249dda88cab6b0823
Kamil Salikhov <kamil@aimfund.ru>
2016-07-15 10:39:58 +0300

    output node names instead of node ids
    Former-commit-id: 2fafa015aa67a88aa090b03b2899477ef6554be8

--- src/kmer-index/contig_node_translator.c
+++ src/kmer-index/contig_node_translator.c
@@ -3,16 +3,20 @@
 #include <stdlib.h>
 #include "contig_node_translator.h"
 
-int contig_to_node[200000000];
-char* node_names[100000];
-int nodes_count = 0;
-int contigs_count = 0;
+static int contig_to_node[200000000];
+static char* node_names[100000];
+static int nodes_count = 0;
+static int contigs_count = 0;
 
 int get_node_from_contig(int contig) {
   if (contig < 0 || contig >= contigs_count) {
     fprintf(stderr, "contig %d is outside of range [%d, %d]\n", contig, 0, contigs_count - 1);
   }
   return contig_to_node[contig];
+}
+
+char* get_node_name(const int node) {
+  return node_names[node];
 }
 
 void add_contig(const char* contig, int contig_number) {
@@ -27,10 +31,6 @@ void add_contig(const char* contig, int contig_number) {
   char* node_name = malloc((index + 1) * sizeof(char));
   memcpy(node_name, contig, index);
   node_name[index] = '\0';
-  // if (nodes_count > 0) {
-  //   fprintf(stderr, "prev node_name = %s\n", node_names[nodes_count - 1]);
-  //   fprintf(stderr, "equal = %d\n", strcmp(node_name, node_names[nodes_count - 1]));
-  // }
   if (nodes_count == 0 || strcmp(node_name, node_names[nodes_count - 1])) {
     node_names[nodes_count] = node_name;
     contig_to_node[contig_number] = nodes_count;
@@ -38,5 +38,5 @@ void add_contig(const char* contig, int contig_number) {
   } else {
     contig_to_node[contig_number] = nodes_count - 1;
   }
-  // fprintf(stderr, "contig = %d, node = %d\n", contig_number, contig_to_node[contig_number]);
+  //fprintf(stderr, "contig = %d, nodes_count = %d\n", contig_number, nodes_count);
 }
--- src/kmer-index/contig_node_translator.h
+++ src/kmer-index/contig_node_translator.h
@@ -4,6 +4,7 @@
 #include <stdint.h>
 
 int get_node_from_contig(int contig);
+char* get_node_name(int node);
 void add_contig(const char* contig, int contig_number);
 
 #endif //CONTIG_NODE_TRANSLATOR_H
