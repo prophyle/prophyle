545a1f28b6c2c121b330aca37bc372ca40bc5727
Karel Břinda <karel.brinda@gmail.com>
2016-07-18 14:32:59 +0200

    Revert "Merge pull request #84 from karel-brinda/kamil/output_node_names"
    This reverts commit ca75922da94cf8c98d881f3bfdc46b58cfdf71fe, reversing
    changes made to 05c507b1282450ac5d498e8026bef2df933d474f.

--- bin/exk_match_1step.py
+++ bin/exk_match_1step.py
@@ -29,15 +29,13 @@ def create_bwa_index(fa):
 def create_klcp(fa, k):
 	cmd('"{exk}" index -k {k} "{fa}"'.format(exk=exk,fa=fa,k=k))
 
-def match(fa, fq, k, s=False, u=False, v=False):
+def match(fa, fq, k, s=False, u=False):
 	params=""
-	if v:
-		params+=" -v"
 	if s:
 		params+=" -s"
 	if u:
 		params+=" -u"
-	cmd('"{exk}" match {params} -k {k} "{fa}" "{fq}"'.format(exk=exk,fa=fa,fq=fq,k=k, params=params),stdout=sys.stdout)
+	cmd('"{exk}" match {params} -v -k {k} "{fa}" "{fq}"'.format(exk=exk,fa=fa,fq=fq,k=k, params=params),stdout=sys.stdout)
 
 parser = argparse.ArgumentParser(description='One command exk matching.')
 parser.add_argument(
@@ -47,11 +45,6 @@ parser.add_argument(
 		dest='k',
 		required=True,
 		help='k-mer length',
-	)
-parser.add_argument(
-		'-v',
-		action='store_true',
-		help='verbose output format',
 	)
 parser.add_argument(
 		'-u',
@@ -80,7 +73,6 @@ fa=args.in_fasta
 fq=args.in_fq
 k=args.k
 u=args.u
-v=args.v
 s=args.s
 
 create_bwa_index(fa)
@@ -90,4 +82,4 @@ if u:
 	#cmd('"{exk}" index -k {k} "{fa}"'.format(exk=exk,fa=args.in_fasta,k=args.k))
 
 #cmd('"{exk}" match -v -k {k} "{fa}" "{fq}"'.format(exk=exk,fa=args.in_fasta,fq=args.in_fq,k=args.k),stdout=sys.stdout)
-match(fa, fq, k, s=s, u=u, v=v)
+match(fa, fq, k, s=s, u=u)
