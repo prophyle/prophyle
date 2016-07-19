b08114339356118e90c53d9939d60555344b5d11
Simone Pignotti <pignottisimone@gmail.com>
2016-07-19 11:59:30 +0200

    Reorganize experiments

---
+++ experiments/01_1from10_bacteria_bin_k31/params.mk
@@ -0,0 +1,2 @@
+TREE=../../trees/1from10.bacteria.bin.newick
+K=31
--- experiments/x0_1from10_bacteria_bin_k10/params.mk
+++ experiments/x0_1from10_bacteria_bin_k10/params.mk
@@ -1,4 +1,4 @@
-TREE=../../trees/1from10.bin.newick
+TREE=../../trees/1from10.bacteria.bin.newick
 K=10
 
 READS=../../reads/simulation_bacteria.1000.fq
