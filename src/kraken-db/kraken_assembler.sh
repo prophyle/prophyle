6d928b1a2665da2a12f0d412111707715da08e08
Simone Pignotti <pignottisimone@gmail.com>
2016-07-19 11:38:39 +0200

    Command line args for krakendb final script
    Former-commit-id: 15ac6556701fc45be81752dc49787589f4372aac

--- src/kraken-db/kraken_assembler.sh
+++ src/kraken-db/kraken_assembler.sh
@@ -1,43 +1,46 @@
 #!/bin/bash
 
-USAGE="kraken_assembler -t taxa_map_file"
+USAGE="Usage: kraken_assembler -t taxa-map-file -d sorted-db \
+		-i sorted-idx -f indexing-dir -k kmer-len"
 
-while [[ $# -gt 1 ]]
-do
-key="$1"
-
-case $key in
-	-t|--taxa-map)
-	TAXA_MAP="$2"
-	shift
-	;;
-	-d|--db)
-	SORTED_DB="$2"
-	shift
-	;;
-	-i|--idx)
-	SORTED_IDX="$2"
-	shift
-	;;
-	*)
-	echo $USAGE
-	exit(-1)
-	;;
-esac
-shift # past argument or value
+while [[ $# -gt 1 ]]; do
+	key=$1
+	case $key in
+		-t|--taxa-map)
+			TAXA_MAP=$2
+			shift
+		;;
+		-d|--db)
+			DB=$2
+			shift
+		;;
+		-i|--idx)
+			IDX=$2
+			shift
+		;;
+		-f|--indexing-dir)
+			INDEXING_DIR=$2
+			if [[ "$INDEXING_DIR" != */ ]]; then
+				INDEXING_DIR="$(INDEXING_DIR)/"
+			fi
+			shift
+		;;
+		-k)
+			K=$2
+			shift
+		;;
+		*)
+			echo $USAGE
+			exit 1
+		;;
+	esac
 done
-echo FILE EXTENSION  = "${EXTENSION}"
-echo SEARCH PATH     = "${SEARCHPATH}"
-echo LIBRARY PATH    = "${LIBPATH}"
-echo "Number files in SEARCH PATH with EXTENSION:" $(ls -1 "${SEARCHPATH}"/*."${EXTENSION}" | wc -l)
-if [[ -n $1 ]]; then
-    echo "Last line of file specified as non-opt/last argument:"
-    tail -1 $1
-fi
 
-# while read line
-# do
-# 	../../bin/assembler \
-# 		-i <() \
-# 		-o /dev/null -x test/0.fa -k 31
-# done < $1
+
+
+while read taxid; do
+	../../bin/assembler \
+		-i <(./get_kmers_by_taxid -t $(taxid) \
+		-d $(DB) -i $(IDX) -m $(TAXA_MAP) ) \
+		-o /dev/null -x $(INDEXING_DIR)$(taxid).fa -k $(K)
+done < <(./taxa_list -m $(TAXA_MAP))
