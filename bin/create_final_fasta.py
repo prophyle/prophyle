2ebb300a1e0db47ef4dd9c7210d49cab5cf6d06f
Karel Břinda <karel.brinda@gmail.com>
2016-07-21 11:06:27 +0200

    Fix node name detection

--- bin/create_final_fasta.py
+++ bin/create_final_fasta.py
@@ -5,8 +5,8 @@ import os
 import glob
 
 if len(sys.argv)!=2:
-    print ("Script has 1 parameter: directory with FASTA files")
-    sys.exit(-1)
+	print ("Script has 1 parameter: directory with FASTA files")
+	sys.exit(-1)
 
 dir_fn=sys.argv[1]
 
@@ -14,19 +14,18 @@ os.chdir(dir_fn)
 fa_fns=glob.glob("*.reduced.fa")
 fa_fns.sort()
 for fn in fa_fns:
-    print("Processing '{}'".format(fn), file=sys.stderr)
+	print("Processing '{}'".format(fn), file=sys.stderr)
 
-    parts=fn.split(".")
-    contig=parts[0]
+	node=fn.replace(".reduced.fa","")
 
-    with open(fn) as f:
-        for x in f:
-            x=x.strip()
-            if x=="":
-                continue
-            if x[0]==">":
-                print (">{}_{}".format(contig,x[1:]))
-            else:
-                print(x)
+	with open(fn) as f:
+		for x in f:
+			x=x.strip()
+			if len(x)==0:
+				continue
+			if x[0]==">":
+				print (">{}_{}".format(node,x[1:]))
+			else:
+				print(x)
 
 
