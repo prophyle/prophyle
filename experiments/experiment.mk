d5a7e490cf9499e04776baeb5dc710ec2cb81108
Karel Břinda <karel.brinda@gmail.com>
2017-01-19 17:31:05 -0500

    Keep only unique lines

--- experiments/experiment.mk
+++ experiments/experiment.mk
@@ -85,7 +85,7 @@ index.fa.tree: index.fa.kmers.tsv $(TREE)
 	# todo: add this to the main prophyle cli script
 	touch $@.kmers.tsv
 	echo "#file	no_kmers" >> $@.kmers.tsv
-	cat index/*.count.tsv | grep -v "^#" | sort >> $@.kmers.tsv
+	cat index/*.count.tsv | grep -v "^#" | sort | uniq >> $@.kmers.tsv
 
 index.fa.pac: index.fa
 	$(TTIME) -o 2.1_bwa_fa2pac.log \
