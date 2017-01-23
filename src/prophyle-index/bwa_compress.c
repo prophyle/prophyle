db3d0dededd061efae68bf2073b4ea02a87459e7
Kamil Salikhov <kamil@aimfund.ru>
2017-01-24 02:24:48 +0300

    wip

--- src/prophyle-index/bwa_compress.c
+++ src/prophyle-index/bwa_compress.c
@@ -25,40 +25,80 @@ void bwt_bwtupdate_core(bwt_t *bwt);
 int bwa_bwtupdate(int argc, char *argv[]);
 int bwa_bwt2sa(int argc, char *argv[]);
 
+#define bwt_B00(b, k) ((b)->bwt[(k)>>4]>>((~(k)&0xf)<<1)&3)
+
+void bwt_revert_bwtupdate_core(bwt_t *bwt)
+{
+	bwtint_t i, k, c[4], n_occ;
+	uint32_t *buf;
+
+	bwt->bwt_size = (bwt->seq_len + 15) >> 4;
+	// n_occ = (bwt->seq_len + OCC_INTERVAL - 1) / OCC_INTERVAL + 1;
+	// bwt->bwt_size += n_occ * sizeof(bwtint_t); // the new size
+	buf = (uint32_t*)calloc(bwt->bwt_size, 4); // will be the new bwt
+	for (i = k = 0; i < bwt->seq_len; ++i) {
+		if (i % OCC_INTERVAL == 0) {
+			k += sizeof(bwtint_t); // in fact: sizeof(bwtint_t)=4*(sizeof(bwtint_t)/4)
+		}
+		if (i % 16 == 0) {
+			buf[i / 16] = bwt->bwt[k]; // 16 == sizeof(uint32_t)/2
+			k++;
+		}
+	}
+	// the last element
+	// update bwt
+	free(bwt->bwt); bwt->bwt = buf;
+}
+
 int bwa_build_compressed_index(const char *fa, const char *prefix, int algo_type, int block_size)
 {
-	char *str, *str2, *str3;
+	char *str, *str2;
 	clock_t t;
 	int64_t l_pac;
 
 	str  = (char*)calloc(strlen(prefix) + 10, 1);
 	str2 = (char*)calloc(strlen(prefix) + 10, 1);
-	str3 = (char*)calloc(strlen(prefix) + 10, 1);
 
-	{ // nucleotide indexing
-		gzFile fp = xzopen(fa, "r");
-		t = clock();
-		if (bwa_verbose >= 3) fprintf(stderr, "[bwa_index] Pack FASTA... ");
-		l_pac = bns_fasta2bntseq(fp, prefix, 0);
-		if (bwa_verbose >= 3) fprintf(stderr, "%.2f sec\n", (float)(clock() - t) / CLOCKS_PER_SEC);
-		err_gzclose(fp);
-	}
-	if (algo_type == 0) algo_type = l_pac > 50000000? 2 : 3; // set the algorithm for generating BWT
+	// { // nucleotide indexing
+	// 	gzFile fp = xzopen(fa, "r");
+	// 	t = clock();
+	// 	if (bwa_verbose >= 3) fprintf(stderr, "[bwa_index] Pack FASTA... ");
+	// 	l_pac = bns_fasta2bntseq(fp, prefix, 0);
+	// 	if (bwa_verbose >= 3) fprintf(stderr, "%.2f sec\n", (float)(clock() - t) / CLOCKS_PER_SEC);
+	// 	err_gzclose(fp);
+	// }
+	//if (algo_type == 0) algo_type = l_pac > 50000000? 2 : 3; // set the algorithm for generating BWT
+	// {
+	// 	strcpy(str, prefix); strcat(str, ".pac");
+	// 	strcpy(str2, prefix); strcat(str2, ".bwt");
+	// 	t = clock();
+	// 	if (bwa_verbose >= 3) fprintf(stderr, "[bwa_index] Construct BWT for the packed sequence...\n");
+	// 	if (algo_type == 2) bwt_bwtgen2(str, str2, block_size);
+	// 	else if (algo_type == 1 || algo_type == 3) {
+	// 		bwt_t *bwt;
+	// 		bwt = bwt_pac2bwt(str, algo_type == 3);
+	// 		fprintf(stderr, "bwt->seq_len = %d\n", bwt->seq_len);
+	// 		fprintf(stderr, "bwt->bwt_size = %d\n", bwt->bwt_size);
+	// 		for (int i = 0; i < bwt->bwt_size && i < 10; ++i) {
+	// 			fprintf(stderr, "%u\n", bwt->bwt[i]);
+	// 		}
+	// 		bwt_dump_bwt(str2, bwt);
+	// 		bwt_destroy(bwt);
+	// 	}
+	// 	if (bwa_verbose >= 3) fprintf(stderr, "[bwa_index] %.2f seconds elapse.\n", (float)(clock() - t) / CLOCKS_PER_SEC);
+	// }
 	{
-		strcpy(str, prefix); strcat(str, ".pac");
-		strcpy(str2, prefix); strcat(str2, ".bwt");
-		t = clock();
-		if (bwa_verbose >= 3) fprintf(stderr, "[bwa_index] Construct BWT for the packed sequence...\n");
-		if (algo_type == 2) bwt_bwtgen2(str, str2, block_size);
-		else if (algo_type == 1 || algo_type == 3) {
-			bwt_t *bwt;
-			bwt = bwt_pac2bwt(str, algo_type == 3);
-			bwt_dump_bwt(str2, bwt);
-			bwt_destroy(bwt);
-		}
-		if (bwa_verbose >= 3) fprintf(stderr, "[bwa_index] %.2f seconds elapse.\n", (float)(clock() - t) / CLOCKS_PER_SEC);
-	}
-	free(str3); free(str2); free(str);
+    bwt_t *bwt;
+    strcpy(str, prefix); strcat(str, ".bwt");
+    t = clock();
+    if (bwa_verbose >= 3) fprintf(stderr, "[bwa_index] Revert update BWT... ");
+    bwt = bwt_restore_bwt(str);
+    bwt_revert_bwtupdate_core(bwt);
+    bwt_dump_bwt(str, bwt);
+    bwt_destroy(bwt);
+    if (bwa_verbose >= 3) fprintf(stderr, "%.2f sec\n", (float)(clock() - t) / CLOCKS_PER_SEC);
+  }
+	free(str); free(str2);
 	return 0;
 }
 
@@ -105,6 +145,8 @@ int bwa_compress(int argc, char *argv[]) // the "compress" command
 	free(prefix);
 	return 0;
 }
+
+#define bwt_B00(b, k) ((b)->bwt[(k)>>4]>>((~(k)&0xf)<<1)&3)
 
 int bwa_decompress_index(const char *fa, const char *prefix) {
   char *str, *str3;
