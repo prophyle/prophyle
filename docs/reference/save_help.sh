30b0241e916867d1be599e905858a6cb9bb99197
Simone Pignotti <pignottisimone@gmail.com>
2017-03-30 17:14:14 +0200

    Add .gitignore; add tutorial; update reference

--- docs/reference/save_help.sh
+++ docs/reference/save_help.sh
@@ -12,3 +12,15 @@ for x in main init index classify ; do
 	echo >> $fn
 	../../bin/prophyle.py $x -h >> $fn
 done
+
+# TODO: solve problem with dashes and output redirection
+# TODO: change assignment.py to prophyle-assignment
+
+# for x in assembler index assignment ; do
+# 	fn="prophyle_$x.txt"
+#		com="prophyle-$x"
+# 	echo "$fn"
+# 	echo "$ $com -h" > $fn
+# 	echo >> $fn
+# 	../../bin/$com >> $fn
+# done
