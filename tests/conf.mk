0ce253a3794de49d089ebea12e0ca2090b9703c6
Kamil Salikhov <kamil@aimfund.ru>
2017-04-11 00:17:56 +0300

    fix testA02

--- tests/conf.mk
+++ tests/conf.mk
@@ -5,5 +5,6 @@ PROP_DIR=../../prophyle
 ASM=$(PROP_DIR)/prophyle_assembler/prophyle_assembler
 IND=$(PROP_DIR)/prophyle-index/prophyle-index
 F2K=$(PROP_DIR)/_fa_to_kmers.py
+AK=$(PROP_DIR)/_all_kmers.py
 1STEP=$(PROP_DIR)/1step_match.py
 TEST_NEWICK=$(PROP_DIR)/test_newick_tree.py
