b9ea3d14430af291a3512f7f87db6c71de9f1dec
Kamil Salikhov <kamil@aimfund.ru>
2017-04-11 00:20:08 +0300

    fix testA04
    Former-commit-id: a92b6e955eea85d499260a30587f5f49f21a1206

--- tests/conf.mk
+++ tests/conf.mk
@@ -6,5 +6,6 @@ ASM=$(PROP_DIR)/prophyle_assembler/prophyle_assembler
 IND=$(PROP_DIR)/prophyle-index/prophyle-index
 F2K=$(PROP_DIR)/_fa_to_kmers.py
 AK=$(PROP_DIR)/_all_kmers.py
+NORM=$(PROP_DIR)/_fa_norm.py
 1STEP=$(PROP_DIR)/1step_match.py
 TEST_NEWICK=$(PROP_DIR)/test_newick_tree.py
