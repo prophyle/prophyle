ce44bd8096ffc9f28b06903dc1ef707cebf89ae8
Kamil Salikhov <kamil@aimfund.ru>
2017-04-10 23:52:14 +0300

    fix testA03
    Former-commit-id: 79fb5aa801dc5af15b38904e6822f644d319e085

--- tests/conf.mk
+++ tests/conf.mk
@@ -4,6 +4,6 @@ PROP_DIR=../../prophyle
 
 ASM=$(PROP_DIR)/prophyle_assembler/prophyle_assembler
 IND=$(PROP_DIR)/prophyle-index/prophyle-index
-F2K=$(PROP_DIR)/_fa_to_kmers.py -m a
+F2K=$(PROP_DIR)/_fa_to_kmers.py
 1STEP=$(PROP_DIR)/1step_match.py
 TEST_NEWICK=$(PROP_DIR)/test_newick_tree.py
