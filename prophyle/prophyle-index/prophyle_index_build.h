c0bd2f168b39bb1e84c7c75c5dd9e6596eea14b1
Kamil Salikhov <salikhov.kamil@gmail.com>
2017-04-12 00:14:35 +0300

    rm prophyle-index
    Former-commit-id: 772b5b10142d16d872bda285ee001b1553d544f8

--- prophyle/prophyle-index/prophyle_index_build.h
+++
@@ -1,8 +0,0 @@
-#ifndef PROPHYLE_INDEX_BUILD_H
-#define PROPHYLE_INDEX_BUILD_H
-
-#include "prophyle_utils.h"
-
-void prophyle_index_build_core(const char *prefix, const prophyle_index_opt_t *opt, int sa_intv);
-
-#endif //PROPHYLE_INDEX_BUILD_H
