6dec84ebb4d2c8f21ccde58df626a6e99cf7a390
Karel Břinda <karel.brinda@gmail.com>
2017-04-10 20:12:24 -0400

    A little update of test conf
    Former-commit-id: 0e789215ef9ddbdc069e5d0d862337e1c6a7b26e

--- tests/conf.mk
+++ tests/conf.mk
@@ -6,9 +6,14 @@ PROP_DIR=../../prophyle
 
 ASM=$(PROP_DIR)/prophyle_assembler/prophyle_assembler
 IND=$(PROP_DIR)/prophyle-index/prophyle-index
+BWA=$(PROP_DIR)/prophyle-index/bwa/bwa
+
+TEST_NEWICK=$(PROP_DIR)/prophyle_test_tree.py
+
 F2K=$(PROP_DIR)/_fa_to_kmers.py
 AK=$(PROP_DIR)/_all_kmers.py
 NORM=$(PROP_DIR)/_fa_norm.py
 1STEP=$(PROP_DIR)/1step_match.py
-TEST_NEWICK=$(PROP_DIR)/prophyle_test_tree.py
-BWA=$(PROP_DIR)/prophyle-index/bwa/bwa
+
+FQ=../simulation_bacteria.1000.fq
+FA=index.fa
