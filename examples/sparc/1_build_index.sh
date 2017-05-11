7a1f102ba328f8686a76079fb66fa46e38a410a6
Karel Břinda <karel.brinda@gmail.com>
2017-05-11 17:31:38 -0400

    Classify pneumo reads
    Former-commit-id: f11c6a02dc2eaa98cb692487c71ebcd83d42fc9b

--- examples/sparc/1_build_index.sh
+++ examples/sparc/1_build_index.sh
@@ -2,9 +2,11 @@
 
 K=25
 
+# download SPARC assemblies
 (cd assemblies && ./download_assemblies.sh)
 
+# create a tree compatible with ProPhyle
 (cd tree && ./create_sparc_tree.py)
 
+# build a ProPhyle index
 prophyle index -k $K -g assemblies ./tree/sparc.nw sparc_index
-
