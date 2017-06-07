a9bd3a64ad9245ab62e80306a000649025144f21
Karel Břinda <karel.brinda@gmail.com>
2017-06-07 14:22:00 -0400

    Run quick tests directly from the repo
    Former-commit-id: 61dedb1e4d45f23f8920b71f4c6337427e56ee9a

--- test_match.sh
+++ test_match.sh
@@ -1,5 +1,7 @@
 #! /usr/bin/env bash -f -e -o pipefail
 
-head -n 4 tests/simulation_bacteria.1000.fq | \
-prophyle classify test_idx -
+prophyle=./prophyle/prophyle.py
+
+head -n 4 tests/simulation_bacteria.1000.fq \
+	| $prophyle classify test_idx -
 
