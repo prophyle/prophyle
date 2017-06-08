5231277ca7540f5b228f87f2fb1e4fded35ef498
Karel Břinda <karel.brinda@gmail.com>
2017-06-08 19:07:17 -0400

    Improve sampling strategy

--- prophyle/prophyle_merge_trees.py
+++ prophyle/prophyle_merge_trees.py
@@ -12,15 +12,16 @@ Examples:
 		$ prophyle_merge_trees.py ~/prophyle/bacteria.nw@562 ecoli.nw
 """
 
-import os
-import sys
 import argparse
-
 import ete3
+import os
+import random
+import sys
 
 sys.path.append(os.path.dirname(__file__))
 import prophylelib as pro
 
+random.seed(42)
 
 def add_prefix(tree, prefix):
 	for node in tree.traverse("postorder"):
@@ -28,7 +29,9 @@ def add_prefix(tree, prefix):
 	return tree
 
 
-def merge_trees(input_trees_fn, output_tree_fn, verbose, add_prefixes):
+def merge_trees(input_trees_fn, output_tree_fn, verbose, add_prefixes, sampling_rate):
+	assert sampling_rate is None or 0.0 <= float(sampling_rate) <= 1.0
+
 	t = ete3.Tree(
 			name="merge_root",
 		)
@@ -38,7 +41,7 @@ def merge_trees(input_trees_fn, output_tree_fn, verbose, add_prefixes):
 			print("Only one tree, don't add any prefix", file=sys.stderr)
 		add_prefixes=False
 
-	for i,x in enumerate(input_trees_fn,1):
+	for i, x in enumerate(input_trees_fn, 1):
 		if verbose:
 			print("Loading '{}'".format(x), file=sys.stderr)
 
@@ -54,6 +57,29 @@ def merge_trees(input_trees_fn, output_tree_fn, verbose, add_prefixes):
 			tree_to_add=add_prefix(tree_to_add, i)
 
 		t.add_child(tree_to_add)
+
+	if sampling_rate is not None:
+		sampling_rate=float(sampling_rate)
+
+		leaves_1=[]
+
+		for node in t.traverse("postorder"):
+			if len(node.children)==0:
+				leaves_1.append(node)
+
+		leaves_2=random.sample(leaves_1, max(round(sampling_rate*len(leaves_1)), 1))
+
+		leaves_to_remove=set(leaves_1) - set(leaves_2)
+
+		if verbose:
+			print("Removing the following leaves: {}".format(", ".join(map(apply(lambda x: x.name, leaves_to_remove)))), file=sys.stderr)
+
+		for node in leaves_to_remove:
+			while len(node.up.children)==1:
+				node=node.up
+			node.detach()
+
+		print("Subsampling the tree with rate {:.4f}, {} leaves out of {} were sampled".format(sampling_rate, len(leaves_2), len(leaves_1)), file=sys.stderr)
 
 	if verbose:
 		print("Writing to '{}'".format(output_tree), file=sys.stderr)
@@ -85,6 +111,14 @@ def parse_args():
 				help='output tree',
 			)
 
+		parser.add_argument('-s',
+				help='rate of sampling the tree [no sampling]',
+				dest='sampling_rate',
+				metavar='FLOAT',
+				type=str,
+				default=None,
+			)
+
 		parser.add_argument('-V',
 				help='verbose',
 				dest='verbose',
@@ -108,6 +142,7 @@ def main():
 			output_tree_fn=args.out_tree,
 			verbose=args.verbose,
 			add_prefixes=args.add_prefixes,
+			sampling_rate=args.sampling_rate,
 		)
 
 
