7668a4f8a5f841e2f2c5165b2d6f5b8f17b81c24
Simone Pignotti <pignottisimone@gmail.com>
2017-06-14 20:32:10 +0200

    Improve parsing and add skeleton for otu_table
    Former-commit-id: a47da1c2a025e5ea698208bd7ec0b9d7dca59eb7

--- prophyle/prophyle_analyze.py
+++ prophyle/prophyle_analyze.py
@@ -20,62 +20,62 @@ import ete3
 
 def parse_args():
 
-    desc = """\
+	desc = """\
 		Program: prophyle_analyze.py
 
 		Analyze results of ProPhyle's classification
-        Stats:
-            (1) Unique assignments, non-weighted
-            (2) Weighted assignments
-            (3) Unique assignments, propagated to leaves
-            (4) Weighted assignments, propagated to leaves
+		Stats:
+			(1) Unique assignments, non-weighted
+			(2) Weighted assignments
+			(3) Unique assignments, propagated to leaves
+			(4) Weighted assignments, propagated to leaves
 	"""
 
 	parser = argparse.ArgumentParser(description=desc)
 
 	parser.add_argument('sam',
-            metavar='<sam_f>'
+			metavar='<sam_f>'
 			type=argparse.FileType('r'),
-            help='ProPhyle output in SAM/BAM format'
+			help='ProPhyle output in SAM/BAM format'
 		)
 
 	parser.add_argument('tree',
-    		metavar='<tree.nw>',
-    		type=str,
-    		help='Newick/NHX tree from the directory of the index used for classification'
+			metavar='<tree.nw>',
+			type=str,
+			help='Newick/NHX tree from the directory of the index used for classification'
 		)
 
 	parser.add_argument('-n','--main-node',
 			type=str,
-            metavar='<node_name>'
+			metavar='<node_name>'
 			dest='main_node_name',
 			default='merge_root',
-            help='Node from which the distances should be computed [root]'
+			help='Node from which the distances should be computed [root]'
 		)
 
 	parser.add_argument('-l','--number-of-records',
 			type=int,
-            metavar='n',
+			metavar='n',
 			dest='lines',
 			default=-1,
-            help='Output only the n nodes with the highest score [all]'
+			help='Output only the n nodes with the highest score [all]'
 		)
 
-    parser.add_argument('-t','--otu',
-            type=str,
-            metavar='<otu_suffix>'
-            dest='otu_suffix',
-            default=None
-            help='Compute OTU tables from the histograms and write them to fn<i>_otu_suffix.biom'
-        )
+	parser.add_argument('-t','--otu',
+			type=str,
+			metavar='<otu_suffix>'
+			dest='otu_suffix',
+			default=None
+			help='Compute OTU tables from the histograms and write them to fn<i>_otu_suffix.biom'
+		)
 
-    parser.add_argument('-h','--from-histogram',
-            type=argparse.FileType('r'),
-            metavar='<histo>',
-            dest='histo',
-            default=None,
-            help='Compute OTU table from existing histogram (do not compute any histogram)'
-        )
+	parser.add_argument('-h','--from-histogram',
+			type=argparse.FileType('r'),
+			metavar='<histo>',
+			dest='histo',
+			default=None,
+			help='Compute OTU table from existing histogram (do not compute any histogram)'
+		)
 
 	for i in range(1,5):
 		parser.add_argument('-{}'.format(i),
@@ -159,60 +159,62 @@ def compute_histograms(tree, asgs):
 	return (h1, h2, h3, h4)
 
 
-def compute_node_distances(tree, n1_name, n2_name):
-	dist1=None
-	dist2=None
+def compute_node_distances(tree, main_node_name):
+	distances={}
 
-	if n1_name=="merge_root":
-		node1=tree
+	if main_node_name=="merge_root":
+		main_node=tree
 	else:
 		try:
-			node1=tree & n1_name
+			main_node=tree & main_node_name
 		except:
-			print("Error node name: {}".format(n1_name), file=sys.stderr)
+			print("[prophyle_analyze] Error: node name {} not in the tree".format(main_node_name),
+					file=sys.stderr)
 			raise
 
-	if n2_name=="merge_root":
-		node2=tree
-	else:
-		try:
-			node2=tree & n2_name
-		except:
-			print("Error node name: {}".format(n2_name), file=sys.stderr)
-			raise
-
-	dist1=node1.get_distance(node2)
-	dist2=node1.get_distance(node2, topology_only=True)
-
-	return (dist1, dist2)
+	for node in tree.iterate('postorder'):
+		dist1=main_node.get_distance(node)
+		dist2=main_node.get_distance(node, topology_only=True)
+		distances[node.name]=(dist1,dist2)
 
 
-def print_histogram(tree, main_node_name, histogram, file, lines):
-	print ("#node", "hits", "dist", "dist_nodes", sep="\t", file=file)
+def print_histogram(histogram, distances, out_file, lines):
+	print ("#node", "hits", "dist", "dist_nodes", sep="\t", file=out_file)
+	# reverse = true???
 	for i, (node_name,w) in enumerate(sorted(histogram.items(), key=operator.itemgetter(1), reverse=True)):
-		if not i < lines:
+		if i > 0 and not i < lines:
 			break
-		dist1,dist2=compute_node_distances(tree, main_node_name, node_name)
-		print(node_name, "{:.2f}".format(round(w,2)) if isinstance(w, float) else w, "{:.2f}".format(round(dist1,2)), dist2, sep="\t", file=file)
+		try:
+			dist1,dist2=distances[node_name]
+			print(node_name, "{:.2f}".format(round(w,2)) if isinstance(w, float) else w,
+					"{:.2f}".format(round(dist1,2)), dist2, sep="\t", file=out_file)
+		except KeyError:
+			print("[prophyle_analyze] Error: node name {} found in assignments but not in the tree".format(n1_name),
+					file=sys.stderr)
+			raise
 
+def compute_otu_tables():
+	return
 
 def main():
 	args=parse_args()
 
-	tree = ete3.Tree(args.tree, format=1)
-	asgs=load_asgs(args.sam)
+	tree=ete3.Tree(args.tree, format=1)
 
-	histograms = compute_histograms(tree, asgs)
-
-	for (histogram, fn) in zip(histograms, (args.fn1, args.fn2, args.fn3, args.fn4) ):
-		if fn is not None:
-			with open(fn, "w+") as f:
-				print_histogram(tree, args.main_node_name, histogram, f, lines=args.lines)
-
+	if args.sam:
+		asgs=load_asgs(args.sam)
+		histograms=compute_histograms(tree, asgs)
+		for (histogram, fn) in zip(histograms, (args.fn1, args.fn2, args.fn3, args.fn4) ):
+			if fn is not None:
+				with open(fn, "w+") as f:
+					print_histogram(tree, args.main_node_name, histogram, f, lines=args.lines)
+	elif args.histo:
+		otu_tables=compute_otu_tables()
 
 if __name__ == "__main__":
 	try:
 		main()
+	# why catch IOError and exit without error???
 	except (BrokenPipeError, IOError):
 		sys.stderr.close()
-exit(0)
+		sys.exit(0)
