d09f5786adc7fcc90dfbd2389d45df57fba92c88
Karel Břinda <karel.brinda@gmail.com>
2017-07-06 09:55:27 -0400

    Fix test3_analyze.sh
    Former-commit-id: 6aaf8d393cb24b08e98bb9c8fc563d403fef41e8

--- test3_analyze.sh
+++ test3_analyze.sh
@@ -1,5 +1,4 @@
 #! /usr/bin/env bash -f -e -o pipefail
 
-samtools view _test_bam1.bam  | ./prophyle/prophyle.py analyze ./_index_test - _test_analyze.basic
-samtools view _test_bam1.bam  | ./prophyle/prophyle.py analyze -N ./_index_test - _test_analyze.ncbi
+./prophyle/prophyle.py analyze ./_index_test _test_analyze _test_bam1.bam
 
