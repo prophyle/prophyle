f5eff4cac1b88a18bdc38dea2617c062bfc7195b
Karel Břinda <kbrinda@hsph.harvard.edu>
2017-07-13 11:08:57 -0400

    Run prophyle from the repository

--- examples/mycobacterium/1_build_index.sh
+++ examples/mycobacterium/1_build_index.sh
@@ -8,7 +8,7 @@ set -o pipefail
 K=31
 
 # download genomes
-prophyle download bacteria
+../../prophyle/prophyle.py download bacteria
 
 # build a ProPhyle index
-prophyle index -k $K ~/prophyle/bacteria.nw@77643 mycobacterium_index
+../../prophyle/prophyle.py index -k $K ~/prophyle/bacteria.nw@77643 mycobacterium_index
