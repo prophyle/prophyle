4242a8f3a389d7002d5a0ea3f26aff4dcdb167fd
Karel Břinda <karel.brinda@gmail.com>
2017-09-11 12:51:36 -0400

    Convert tabs to spaces

--- prophyle/tree_print_fasta_names.py
+++ prophyle/tree_print_fasta_names.py
@@ -10,29 +10,29 @@ import prophylelib as pro
 
 
 class TreeIndex:
-	def __init__(self, tree_newick_fn):
-		self.tree_newick_fn = tree_newick_fn
-		self.tree = pro.load_nhx_tree(tree_newick_fn)
+    def __init__(self, tree_newick_fn):
+        self.tree_newick_fn = tree_newick_fn
+        self.tree = pro.load_nhx_tree(tree_newick_fn)
 
-	def process_node(self, node):
-		if node.is_leaf():
-			if hasattr(node, "fastapath"):
-				fastas_fn = node.fastapath.split("@")
-				for fasta_fn in fastas_fn:
-					print(fasta_fn)
+    def process_node(self, node):
+        if node.is_leaf():
+            if hasattr(node, "fastapath"):
+                fastas_fn = node.fastapath.split("@")
+                for fasta_fn in fastas_fn:
+                    print(fasta_fn)
 
-		else:
-			children = node.get_children()
+        else:
+            children = node.get_children()
 
-			for child in children:
-				self.process_node(child)
+            for child in children:
+                self.process_node(child)
 
 
 if __name__ == "__main__":
-	assert (len(sys.argv) == 2)
-	newick_fn = sys.argv[1]
+    assert (len(sys.argv) == 2)
+    newick_fn = sys.argv[1]
 
-	ti = TreeIndex(
-		tree_newick_fn=newick_fn,
-	)
-	ti.process_node(ti.tree.get_tree_root())
+    ti = TreeIndex(
+        tree_newick_fn=newick_fn,
+    )
+    ti.process_node(ti.tree.get_tree_root())
--- tests/testA11_check_results/create_tree.py
+++ tests/testA11_check_results/create_tree.py
@@ -3,5 +3,5 @@ from ete3 import Tree
 t = Tree('((n1,n2,n3)n4,(n5,n6)n7);', format=1)
 t.name='n8'
 for node in t:
-	node.add_features(fastapath='fastas/'+node.name+'.fa',seqname=node.name+'_seq')
+    node.add_features(fastapath='fastas/'+node.name+'.fa',seqname=node.name+'_seq')
 t.write(format=1,features=['fastapath','seqname'],outfile='tree.nw',format_root_node=True)
