c57c92b2cc60bfaca787a560f4f758fb35bd6559
Karel Břinda <karel.brinda@gmail.com>
2017-09-30 19:02:11 -0400

    Refactoring (not finished)

--- prophyle/prophyle_assignment.py
+++ prophyle/prophyle_assignment.py
@@ -1,23 +1,18 @@
 #! /usr/bin/env python3
 
-"""ProPhyle assignment algorithm (Python reference implementation).
+"""ProPhyle assignment algorithm (reference implementation).
 
 Author:  Karel Brinda <kbrinda@hsph.harvard.edu>
 
 License: MIT
-
-TODO:
-    * Allow to use c2 and h2 (when k-mer annotations exist in the NHX tree)
 """
 
-import os
-import shutil
-import datetime
-import sys
 import argparse
-import bitarray
 import itertools
+import os
+import sys
 
+import bitarray
 import ete3
 
 sys.path.append(os.path.dirname(__file__))
@@ -39,7 +34,7 @@ def cigar_from_mask(mask):
     """
 
     c = []
-    mask_bin=map(bool,mask)
+    mask_bin = map(bool, mask)
     runs = itertools.groupby(mask_bin)
     for run in runs:
         c.append(str(len(list(run[1]))))
@@ -50,15 +45,23 @@ def cigar_from_mask(mask):
 class Read:
     """Class for handling a single read.
 
-    Attributes:
+    Args:
         output (file): Output file object.
         tree (ete3.Tree): Phylogenetic tree.
         simulate_lca (bool): Simulate LCA on the k-mer level.
-        tie_lca (bool): If a tie (multiple best nodes), compute LCA.
+        tie_lca (bool): If a tie (multiple winning nodes), compute LCA.
+        annotate (bool): If taxonomic info present in the tree, annotate the assignments using SAM tags.
+        dont_translate_blocks (bool): ?
+
+    Attributes:
+        output (file): Output file object.
+        tree (ete3.Tree): Phylogenetic tree.
+        k (int): k-mer size.
+        simulate_lca (bool): Simulate LCA on the k-mer level.
+        tie_lca (bool): If a tie (multiple winning nodes), compute LCA.
         annotate (bool): If taxonomic info present in the tree, annotate the assignments using SAM tags.
         dont_translate_blocks (bool): ?
     """
-
 
     def __init__(self, output, tree, simulate_lca=False, tie_lca=False, annotate=False, dont_translate_blocks=False):
         self.output = output
@@ -69,6 +72,13 @@ class Read:
         self.tie_lca = tie_lca
         self.dont_translate_blocks = dont_translate_blocks
 
+        self.qname = None
+        self.krakmers = parts[1:5]
+        self.qlen = int(qlen)
+        self.seq = None
+        self.qual = None
+        self.asgs = {}
+        self.kmer_blocks = []
 
     def process_krakline(self, krakline, form, measure):
         """Process one Kraken-like line.
@@ -84,7 +94,6 @@ class Read:
         # print(self.asgs)
         self.filter_assignments(measure)
         self.print_assignments(form, measure)
-
 
     def load_krakline(self, krakline):
         """Load a krakline to the current object.
@@ -120,13 +129,12 @@ class Read:
             self.kmer_blocks.append((rnames, count))
         assert self.qlen == b_sum + self.k - 1 or self.qlen < self.k, krakline
 
-
-    def find_compute_masks(self):
+    def compute_masks(self):
         """Compute possible assignments of the loaded read.
         """
 
         # hits before top-down propagation
-        hitmasks, covmasks = self.tree.masks_from_kmer_blocks(self.kmer_blocks, simulate_lca=self.simulate_lca)
+        hitmasks, covmasks = self.tree.masks_from_kmer_blocks(self.kmer_blocks, simulate_kmer_lca=self.simulate_kmer_lca)
 
         rnames = hitmasks.keys()
 
@@ -141,7 +149,6 @@ class Read:
             for p_rname in self.tree.upnodes_dict[rname] & rnames:
                 self.asgs[rname]['hitmask'] |= hitmasks[p_rname]
                 self.asgs[rname]['covmask'] |= covmasks[p_rname]
-
 
     def filter_assignments(self, measure):
         """
@@ -164,16 +171,16 @@ class Read:
             """
             hit = asg['hitmask'].count()
             asg['h1'] = hit
-            asg['hf'] = hit/(self.qlen-self.k+1)
-            asg['h2'] = hit/self.tree.kmer_count_dict[rname]
+            asg['hf'] = hit / (self.qlen - self.k + 1)
+            asg['h2'] = hit / self.tree.kmer_count_dict[rname]
 
             """
             2. coverage
             """
             cov = asg['covmask'].count()
             asg['c1'] = cov
-            asg['cf'] = cov/self.qlen
-            asg['c2'] = cov/self.tree.kmer_count_dict[rname]
+            asg['cf'] = cov / self.qlen
+            asg['c2'] = cov / self.tree.kmer_count_dict[rname]
 
             """
             3. other values
@@ -189,11 +196,10 @@ class Read:
             elif asg[measure] == self.max_val:
                 self.max_rnames.append(rname)
 
-        for i,rname in enumerate(self.max_rnames):
+        for i, rname in enumerate(self.max_rnames):
             asg = self.asgs[rname]
-            asg['ii']=i+1
-            asg['is']=len(self.max_rnames)
-
+            asg['ii'] = i + 1
+            asg['is'] = len(self.max_rnames)
 
     def print_assignments(self, form, measure):
         """Print the best assignments.
@@ -206,7 +212,7 @@ class Read:
         winners = self.max_rnames
 
         # No assignments
-        if len(winners)==0:
+        if len(winners) == 0:
             if form == "sam":
                 self.print_sam_line(None, "")
             elif form == "kraken":
@@ -220,8 +226,8 @@ class Read:
             #
             # multiple winners => compute LCA and set only those values that are known
             if len(winners) > 1:
-                first_winner=self.asgs[winners[0]]
-                print("winner rec" ,first_winner, file=sys.stderr)
+                first_winner = self.asgs[winners[0]]
+                print("winner rec", first_winner, file=sys.stderr)
                 tie_solved = True
                 lca = self.tree.lca(winners)
                 winners = [lca]
@@ -229,11 +235,11 @@ class Read:
                 asg = self.asgs[lca] = {}
                 asg['covmask'] = None
                 asg['hitmask'] = None
-                #asg['covcigar'] = None
+                # asg['covcigar'] = None
 
-                #asg['hitcigar'] = None
+                # asg['hitcigar'] = None
 
-                if measure=="h1" or measure=="h2":
+                if measure == "h1" or measure == "h2":
                     asg['h1'] = first_winner['h1']
                     asg['h2'] = first_winner['h2']
                     asg['hf'] = first_winner['hf']
@@ -242,7 +248,7 @@ class Read:
                     asg['c2'] = None
                     asg['cf'] = None
 
-                elif measure=="c1" or measure=="c2":
+                elif measure == "c1" or measure == "c2":
                     asg['c1'] = first_winner['c1']
                     asg['c2'] = first_winner['c2']
                     asg['cf'] = first_winner['cf']
@@ -264,94 +270,91 @@ class Read:
                 else:
                     asg['covcigar'] = cigar_from_mask(asg['covmask'])
 
-
                 if asg['hitmask'] is None:
                     asg['hitcigar'] = None
                 else:
                     asg['hitcigar'] = cigar_from_mask(asg['hitmask'])
 
-#                if self.tie_lca and len(winners) > 1:
-#                    asg['covcigar'] = None
-#                    asg['hitcigar'] = None
-#                else:
-#                    print(winners,file=sys.stderr)
-#                    print(asg,file=sys.stderr)
-#                    print(self.asgs,file=sys.stderr)
-#
-#                    if asg['covmask'] is not None:
-#                        asg['covcigar'] = cigar_from_mask(asg['covmask'])
-#                    asg['hitcigar'] = cigar_from_mask(asg['hitmask'])
+                # if self.tie_lca and len(winners) > 1:
+                #                    asg['covcigar'] = None
+                #                    asg['hitcigar'] = None
+                #                else:
+                #                    print(winners,file=sys.stderr)
+                #                    print(asg,file=sys.stderr)
+                #                    print(self.asgs,file=sys.stderr)
+                #
+                #                    if asg['covmask'] is not None:
+                #                        asg['covcigar'] = cigar_from_mask(asg['covmask'])
+                #                    asg['hitcigar'] = cigar_from_mask(asg['hitmask'])
                 self.print_sam_line(rname, self.tree.sam_annotations_dict[rname] if self.annotate else "")
             elif form == "kraken":
                 self.print_kraken_line(rname)
 
-
-    def print_sam_line(self, rname, suffix):
+    def print_sam_line(self, node_name, suffix):
         """Print a single SAM record.
 
         Args:
-            rname (str): Node name. None if unassigned.
+            node_name (str): Node name. None if unassigned.
             suffix (str): Suffix with additional tags.
         """
 
         tags = []
         qname = self.qname
 
-        if rname:
+        if node_name:
             flag = 0
             mapq = "60"
             pos = "1"
-            cigar = self.asgs[rname]['covcigar']
+            cigar = self.asgs[node_name]['covcigar']
         else:
             flag = 4
-            rname = None
+            node_name = None
             pos = None
             mapq = None
             cigar = None
 
         columns = [
-            qname, # QNAME
-            str(flag), # FLAG
-            rname if rname else "*", # RNAME
-            pos if pos else "0", # POS
-            mapq if mapq else "0", # MAPQ
-            cigar if cigar else "*", # CIGAR
-            "*", # RNEXT
-            "0", # PNEXT
-            "0", # TLEN
-            self.seq if self.seq else "*", # SEQ
-            self.qual if self.qual else "*", # QUAL
+            qname,  # QNAME
+            str(flag),  # FLAG
+            node_name if node_name else "*",  # RNAME
+            pos if pos else "0",  # POS
+            mapq if mapq else "0",  # MAPQ
+            cigar if cigar else "*",  # CIGAR
+            "*",  # RNEXT
+            "0",  # PNEXT
+            "0",  # TLEN
+            self.seq if self.seq else "*",  # SEQ
+            self.qual if self.qual else "*",  # QUAL
         ]
 
-        if rname is not None:
-            asg = self.asgs[rname]
+        if node_name is not None:
+            asg = self.asgs[node_name]
 
-            if asg['h1']:
+            if asg['h1'] is not None:
                 columns.append("h1:i:{}".format(asg['h1']))
 
-            if asg['h2']:
+            if asg['h2'] is not None:
                 columns.append("h2:f:{}".format(asg['h2']))
 
-            if asg['hf']:
+            if asg['hf'] is not None:
                 columns.append("hf:f:{}".format(asg['hf']))
 
-            if asg['c1']:
+            if asg['c1'] is not None:
                 columns.append("c1:i:{}".format(asg['c1']))
 
-            if asg['c2']:
+            if asg['c2'] is not None:
                 columns.append("c2:f:{}".format(asg['c2']))
 
-            if asg['cf']:
+            if asg['cf'] is not None:
                 columns.append("cf:f:{}".format(asg['cf']))
 
-
-            if asg['ln']:
+            if asg['ln'] is not None:
                 columns.append("ln:i:{}".format(asg['ln']))
 
-            if asg['ii']:
+            if asg['ii'] is not None:
                 columns.append("ii:i:{}".format(asg['ii']))
 
-            if asg['is']:
+            if asg['is'] is not None:
                 columns.append("is:i:{}".format(asg['is']))
 
             if asg['hitcigar']:
@@ -363,9 +366,8 @@ class Read:
     def print_sam_header(self):
         """Print SAM headers.
         """
-
-        print("@PG","PN:prophyle","ID:prophyle", "VN:{}".format(version.VERSION), sep="\t", file=self.output)
-        print("@HD","VN:1.5","SO:unsorted", sep="\t", file=self.output)
+        print("@PG", "PN:prophyle", "ID:prophyle", "VN:{}".format(version.VERSION), sep="\t", file=self.output)
+        print("@HD", "VN:1.5", "SO:unsorted", sep="\t", file=self.output)
         for node in self.tree.tree.traverse("postorder"):
             self.tree.name_dict[node.name] = node
 
@@ -394,28 +396,30 @@ class Read:
                 ), file=self.output)
 
 
-    def print_kraken_line(self, rname):
-        """Print a single Kraken-like record.
+    # TODO: deeply check the entire functino, 
+    def print_kraken_line(self, node_name):
+        """Print a single record in Kraken format.
 
         Args:
-            rname (str): Node name. None is unassigned.
+            node_name (str): Node name. None if unassigned.
         """
 
-        if rname is None:
+        if node_name is None:
             stat = "U"
-            rname = "0"
+            node_name = "0"
         else:
             stat = "C"
 
-        if self.simulate_lca:
+        if self.assignment_lca:
             lca_rnames = []
-            for [rnames, count] in self.kmer_blocks:
+            for [node_names, count] in self.kmer_blocks:
                 assert len(rnames) == 1
 
-                if rnames[0] == "A" or rnames[0] == "0" or self.dont_translate_blocks:
-                    taxid = rnames[0]
+                # todo: ???????
+                if node_names[0] == "A" or node_names[0] == "0" or self.dont_translate_blocks:
+                    taxid = node_names[0]
                 else:
-                    taxid = int(self.tree.taxid_dict[rnames[0]])
+                    taxid = int(self.tree.taxid_dict[node_names[0]])
                 lca_rnames.extend(count * [taxid])
             c = []
             runs = itertools.groupby(lca_rnames)
@@ -431,19 +435,35 @@ class Read:
             else:
                 taxid = "0"
 
+            # todo: check (why is the foramt different ... taxid?)
             columns = [stat, self.qname, taxid, str(self.qlen), pseudokrakenmers]
         # columns=[stat,self.qname,rname,str(self.qlen)," ".join([ "{}:{}".format(",".join(x[0]),x[1]) for x in self.kmer_blocks])]
         else:
-            columns = [stat, self.qname, rname, str(self.qlen), self.krakmers]
+            columns = [stat, self.qname, node_name, str(self.qlen), self.krakmers]
 
         print("\t".join(columns), file=self.output)
 
 
 class TreeIndex:
+    """Class for a Phylogenetic tree.
+
+    Args:
+        tree_newick_fn (str): Filename fo the phylogenetic tree.
+        k (int): K-mer size.
+
+    Attributes:
+        tree (ete3.Tree): Minimal subtree of the original phylogenetic tree.
+        k (int): K-mer size.
+        name_dict (dict): node name => node.
+        taxid_dict (dict): node name => taxid (annotations from the tree).
+        sam_annotations_dict (dict): node name => string to append in SAM.
+        upnodes_dict (dict): node name => set of node names of ancestors.
+        kmer_count_dict (dict): nname => number of k-mers (full set).
+    """
+
     def __init__(self, tree_newick_fn, k):
-        self.tree_newick_fn = tree_newick_fn
         tree = pro.load_nhx_tree(tree_newick_fn)
-        self.tree = pro.minimal_subtree (tree)
+        self.tree = pro.minimal_subtree(tree)
 
         self.k = k
 
@@ -502,57 +522,70 @@ class TreeIndex:
             ]
     """
 
-    def masks_from_kmer_blocks(self, kmers_assigned_l, simulate_lca=False):
-        d_h = {}
-        d_c = {}
+    def masks_from_kmer_blocks(self, kmers_assigned, simulate_kmer_lca=False):
+    """Extract hit and coverage masks from krakline blocks (without propagation).
 
-        npos = sum([x[1] for x in kmers_assigned_l])
+    Args:
+        kmers_assigned (list): List of assigned k-mers, i.e., list of (list of node_names, count).
+        simulate_kmer_lca (bool): Simulate k-mer LCA on the k-mer level.
 
-        h_len = npos
-        c_len = npos + self.k - 1
+    Returns:
+        hit_masks_dict (dict): Dict of hit masks.
+        cov_masks_dict (dict): Dict of coverage masks.
+    """
+
+        npos = sum([x[1] for x in kmers_assigned])
+
+        hit_mask_len = npos
+        cov_mask_len = npos + self.k - 1
+
+        hit_masks_dict = collections.defaultdict(bitarray.bitarray(hit_mask_len*[False]))
+        cov_masks_dict = collections.defaultdict(bitarray.bitarray(cov_mask_len*[False]))
 
         pos = 0
-        for (rname_l, count) in kmers_assigned_l:
-            if rname_l != ["0"] and rname_l != ["A"]:
-                v_h = bitarray.bitarray(pos * [False] + count * [True] + (npos - pos - count) * [False])
-                v_c = bitarray.bitarray(pos * [False] + (count + self.k - 1) * [True] + (npos - pos - count) * [False])
+        for (node_names, count) in kmers_assigned:
+            if node_names != ["0"] and node_names != ["A"]:
 
-                for rname in rname_l:
-                    try:
-                        d_h[rname] |= v_h
-                        d_c[rname] |= v_c
-                    except KeyError:
-                        d_h[rname] = v_h.copy()
-                        d_c[rname] = v_c.copy()
+                if simulate_kmer_lca:
+                    node_names=[self.lca(*node_names)]
+
+                hit_mask_1block = bitarray.bitarray(pos * [False] + count * [True] + (npos - pos - count) * [False])
+                cov_mask_1block = bitarray.bitarray(pos * [False] + (count + self.k - 1) * [True] + (npos - pos - count) * [False])
+
+                for node_name in node_names:
+                    hit_masks_dict[node_name] |= hit_mask_1block
+                    cov_masks_dict[node_name] |= cov_mask_1block
 
             pos += count
-        return (d_h, d_c)
 
-    def lca(self, noden_l):
+        return (hit_masks_dict, cov_masks_dict)
+
+
+    def lca(self, *node_names):
         """Return LCA for a given list of nodes.
         """
-        assert len(noden_l) > 0, noden_l
-        if len(noden_l) == 1:
-            return noden_l[0]
-        nodes_l = list(map(lambda x: self.name_dict[x], noden_l))
-        lca = nodes_l[0].get_common_ancestor(nodes_l)
+        assert len(node_names) > 0
+        if len(node_names) == 1:
+            return node_names[0]
+        nodes = list(map(lambda x: self.name_dict[x], node_names))
+        lca = nodes[0].get_common_ancestor(nodes)
 
         if lca.is_root() and len(lca.children) == 1:
             lca = lca.children[0]
-        assert lca.name != "", [x.name for x in lca.children]
+        assert lca.name != "" #, [x.name for x in lca.children]
         return lca.name
 
 
-def assign(
-        tree_fn,
-        inp_fo,
-        lca,
-        form,
-        k,
-        measure,
-        annotate,
-        tie,
-        dont_translate_blocks,
+def process_all_reads(
+    tree_fn,
+    inp_fo,
+    lca,
+    form,
+    k,
+    measure,
+    annotate,
+    tie,
+    dont_translate_blocks,
 ):
     assert form in ["kraken", "sam"]
     assert k > 1
@@ -653,7 +686,7 @@ def main():
     d = args.donttransl
 
     try:
-        assign(
+        process_all_reads(
             tree_fn=newick_fn,
             inp_fo=inp_fo,
             lca=lca,
@@ -681,6 +714,7 @@ def main():
     finally:
         sys.stdout.flush()
         sys.stderr.flush()
+
 
 if __name__ == "__main__":
     main()
