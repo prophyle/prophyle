4b4264ca9a66961cade3a90aeddda44e29ef890a
Karel Břinda <karel.brinda@gmail.com>
2017-10-03 14:02:52 -0400

    Fix verbosity levels

--- test2_match.sh
+++ test2_match.sh
@@ -8,11 +8,15 @@ echo
 echo " =========== 1 ============"
 echo
 
+set -o verbose
 ./prophyle/prophyle.py classify -P _index_test tests/simulation_bacteria.1000.fq | samtools view -b > _test_bam1.bam
+set +o verbose
 
 echo
 echo " =========== 2 ============"
 echo
 
+set -o verbose
 ./prophyle/prophyle.py classify -P _index_test tests/simulation_bacteria.1000.fq tests/simulation_bacteria.1000.fq | samtools view -b > _test_bam2.bam
+set +o verbose
 
