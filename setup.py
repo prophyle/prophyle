5285c13d9e5f05469ff4ec55eb9a723b4c26bb4f
Karel Břinda <karel.brinda@gmail.com>
2017-11-11 12:28:31 -0500

    Add download url

--- setup.py
+++ setup.py
@@ -1,5 +1,4 @@
 # see https://github.com/pypa/sampleproject
-
 """Installation script for ProPhyle.
 
 To include binaries into the package, run make and set the system variable
@@ -12,11 +11,12 @@ import os
 import setuptools
 import sys
 
-if sys.version_info < (3,2):
-    sys.exit('Minimum supported Python version is 3.2')
+if sys.version_info < (3, 4):
+    sys.exit('Minimum supported Python version is 3.4')
 
-bwa_dir='prophyle/prophyle_index/bwa'
-if len(glob.glob(os.path.join(bwa_dir, "*.c")))==0 or len(glob.glob(os.path.join(bwa_dir, "*.c")))==0:
+bwa_dir = 'prophyle/prophyle_index/bwa'
+if len(glob.glob(os.path.join(bwa_dir, "*.c"))) == 0 or len(
+        glob.glob(os.path.join(bwa_dir, "*.c"))) == 0:
     sys.exit("BWA submodule is missing. Run 'make submodules' to download it.")
 
 here = os.path.abspath(os.path.dirname(__file__))
@@ -25,66 +25,59 @@ here = os.path.abspath(os.path.dirname(__file__))
 with open(os.path.join(here, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
 
-
 # Get the current version
 exec(open("prophyle/version.py").read())
 
 try:
-    packbin=os.environ['PROPHYLE_PACKBIN']
+    packbin = os.environ['PROPHYLE_PACKBIN']
 except KeyError:
-    packbin=False
+    packbin = False
 
 if packbin:
     print("Adding executables and *.o files to the package", file=sys.stderr)
 
-prophyle_files=[
-            'Makefile',
-            '*.py',
-            'prophyle_assembler/*.cpp',
-            'prophyle_assembler/*.h',
-            'prophyle_assembler/Makefile',
-            'prophyle_index/*.c',
-            'prophyle_index/*.h',
-            'prophyle_index/Makefile',
-            'prophyle_index/bwa/*.c',
-            'prophyle_index/bwa/*.h',
-            'prophyle_index/bwa/Makefile',
-            'prophyle_assignment/*.cpp',
-            'prophyle_assignment/*.c',
-            'prophyle_assignment/*.h',
-            'prophyle_assignment/Makefile',
-            'trees/*.nw',
-        ]+(
-            [
-                'prophyle_index/prophyle_index',
-                'prophyle_index/*.o',
-                'prophyle_assembler/prophyle_assembler',
-                'prophyle_assembler/*.o',
-                'prophyle_assignment/prophyle_assignment',
-                'prophyle_assignment/*.o',
-                'prophyle_index/bwa/bwa',
-                'prophyle_index/bwa/*.o',
-                ]
-            if packbin else
-            []
-        )
+prophyle_files = [
+    'Makefile',
+    '*.py',
+    'prophyle_assembler/*.cpp',
+    'prophyle_assembler/*.h',
+    'prophyle_assembler/Makefile',
+    'prophyle_index/*.c',
+    'prophyle_index/*.h',
+    'prophyle_index/Makefile',
+    'prophyle_index/bwa/*.c',
+    'prophyle_index/bwa/*.h',
+    'prophyle_index/bwa/Makefile',
+    'prophyle_assignment/*.cpp',
+    'prophyle_assignment/*.c',
+    'prophyle_assignment/*.h',
+    'prophyle_assignment/Makefile',
+    'trees/*.nw',
+] + ([
+    'prophyle_index/prophyle_index',
+    'prophyle_index/*.o',
+    'prophyle_assembler/prophyle_assembler',
+    'prophyle_assembler/*.o',
+    'prophyle_assignment/prophyle_assignment',
+    'prophyle_assignment/*.o',
+    'prophyle_index/bwa/bwa',
+    'prophyle_index/bwa/*.o',
+] if packbin else [])
 
 setuptools.setup(
     name='prophyle',
-
     version=VERSION,
-
     description='ProPhyle metagenomic classifier',
-
     long_description=long_description,
-
+    #
     url='https://github.com/karel-brinda/prophyle',
-
+    download_url="https://github.com/karel-brinda/prophyle/releases",
+    #
     author='Karel Brinda, Kamil Salikhov, Simone Pignotti, Gregory Kucherov',
-    author_email='kbrinda@hsph.harvard.edu, salikhov.kamil@gmail.com, pignottisimone@gmail.com, gregory.kucherov@univ-mlv.fr',
-
+    author_email=
+    'kbrinda@hsph.harvard.edu, salikhov.kamil@gmail.com, pignottisimone@gmail.com, gregory.kucherov@univ-mlv.fr',
     license='MIT',
-
+    #
     classifiers=[
         'Development Status :: 4 - Beta',
         'Programming Language :: Python :: 3 :: Only',
@@ -94,26 +87,14 @@ setuptools.setup(
         'Intended Audience :: Science/Research',
         'Topic :: Scientific/Engineering :: Bio-Informatics',
     ],
-
+    #
     keywords='metagenomics classification NGS',
-
-    packages = ["prophyle"],
-
-    install_requires=[
-            'ete3',
-            'wheel',
-            'bitarray',
-            'psutil',
-            'pysam',
-            'scipy',
-            'six',
-        ],
-
-    package_data={
-        'prophyle': prophyle_files
-    },
-
-   entry_points={
+    #
+    packages=["prophyle"],
+    #
+    package_data={'prophyle': prophyle_files},
+    #
+    entry_points={
         'console_scripts': [
             'prophyle = prophyle.prophyle:main',
             'prophyle_analyze.py = prophyle.prophyle_analyze:main',
@@ -128,4 +109,15 @@ setuptools.setup(
             'prophyle_split_allseq.py = prophyle.prophyle_split_allseq:main',
         ],
     },
+    #
+    install_requires=[
+        'ete3',
+        'wheel',
+        'bitarray',
+        'psutil',
+        'pysam',
+        'scipy',
+        'six',
+    ],
+    #
 )
