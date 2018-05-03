1a7bf81588a4f3254cbf1f54943f3b6b21fa13df
Simone Pignotti <pignottisimone@gmail.com>
2018-05-03 14:29:36 -0400

    Change fastapath attr name to path

--- prophyle/prophyle_propagation_makefile.py
+++ prophyle/prophyle_propagation_makefile.py
@@ -244,8 +244,8 @@ class TreeIndex:
 
         if node.is_leaf():
 
-            if hasattr(node, "fastapath"):
-                fastas_fn = node.fastapath.split("@")
+            if hasattr(node, "path"):
+                fastas_fn = node.path.split("@")
                 for i in range(len(fastas_fn)):
                     fastas_fn[i] = os.path.join(self.library_dir, fastas_fn[i])
                 merge_fasta_files(fastas_fn, self.nonreduced_fasta_fn(node), is_leaf=True, makefile_fo=makefile_fo)
