[abund] Fix sim matrix computation

prophyle/prophyle_sim_matrix.py
@@ -22,6 +22,7 @@ CONFIG = {
 import os
 import sys
 import numpy
+import pysam
 import argparse
 import multiprocessing
 import concurrent.futures
@@ -34,31 +35,65 @@ import version
 DEFAULT_THREADS = multiprocessing.cpu_count()
 
 
-def analyse_assignments(sam_fn, nodes2leaves, vec_pos):
+def analyse_assignments(leaf_idx, ass_fn_list, nodes2leaves, vec_pos):
 
-    assignments = np.array(len(leaves))
-    sam_f = pysam.AlignmentFile(sam_fn)
-    prev_read_name = ""
-    cur_ref = []
+    assignments = np.zeros(len(vec_pos))
 
-    for read in sam_f.fetch(until_eof=True):
-        if not read.is_unmapped:
-            read_name = read.qname
-            read_ref = read.reference_name
-            if read_name == prev_read_name:
-                cur_ref.append(read_ref)
-            else:
-                for ref in cur_ref:
-                    for leaf in nodes2leaves[ref]:
-                        assignments[vec_pos[leaf]] += 1
-                cur_ref = [read_ref]
-                prev_read_name = read_name
+    for ass_fn in ass_fn_list:
+        ass_f, in_format = pro.open_asg(ass_fn)
+        prev_read_name = ""
+        cur_ref = []
 
-    # last assignment
-    if len(cur_ref) > 0:
-        for ref in cur_ref:
-            for leaf in nodes2leaves[ref]:
-                vec_shared[vec_pos[leaf]] += 1
+        if in_format == 'kraken':
+            for read in ass_f:
+                fields = read.split('\t')
+                if fields[0] == 'C':
+                    read_name = fields[2]
+                    read_ref = fields[3]
+                    if read_name == prev_read_name:
+                        cur_ref.append(read_ref)
+                    else:
+                        for ref in cur_ref:
+                            for leaf in nodes2leaves[ref]:
+                                assignments[vec_pos[leaf]] += 1
+                        cur_ref = [read_ref]
+                        prev_read_name = read_name
+        else:
+            for read in ass_f.fetch(until_eof=True):
+                if not read.is_unmapped:
+                    read_name = read.qname
+                    read_ref = read.reference_name
+                    if read_name == prev_read_name:
+                        cur_ref.append(read_ref)
+                    else:
+                        for ref in cur_ref:
+                            for leaf in nodes2leaves[ref]:
+                                assignments[vec_pos[leaf]] += 1
+                        cur_ref = [read_ref]
+                        prev_read_name = read_name
+
+        # last assignment
+        if len(cur_ref) > 0:
+            for ref in cur_ref:
+                for leaf in nodes2leaves[ref]:
+                    assignments[vec_pos[leaf]] += 1
+
+        ass_f.close()
+
+    # if assignments files don't exist, set the corresponding column to
+    # 0 0 .. 1[i] .. 0 0
+    if sum(assignments) == 0:
+        assignments[leaf_idx] = 1
+
+    ass_to_this = assignments[leaf_idx]
+    # normalize
+    assignments /= ass_to_this
+
+    # check that the right leaf has the highest number of assignments
+    for i, a in enumerate(assignments):
+        if ass_to_this < a:
+            print("[prophyle_sim_matrix] Warning: leaf {} has less assignments than leaf {} for its own simulations; going to reduce its entry to 1".format(leaf_idx, i), file=sys.stderr)
+            assignments[i] = 1.
 
     return assignments
 
@@ -69,8 +104,7 @@ def compute_sim_matrix(tree_fn, lib_dir, out_fn, jobs):
     assigned_fns = []
 
     nodes2leaves = {node.name: {leaf.name for leaf in node} for node in tree.traverse("postorder")}
-    leaves = [leaf.name for leaf in tree]
-    vec_pos = {leaf: i for i, leaf in enumerate(leaves)}
+    vec_pos = {leaf.name: i for i, leaf in enumerate(tree)}
 
     for leaf in tree:
         if hasattr(leaf, 'path'):
@@ -78,23 +112,42 @@ def compute_sim_matrix(tree_fn, lib_dir, out_fn, jobs):
         elif hasattr(leaf, 'fastapath'):
             path = leaf.fastapath
         else:
-            print('[prophyle_rnfsim] Warning: leaf {} has no path or fastapath attribute'.format(leaf.name), file=sys.stderr)
+            print('[prophyle_sim_matrix] Warning: leaf {} has no path or fastapath attribute'.format(leaf.name), file=sys.stderr)
             continue
 
-        assert '@' not in path, "[prophyle_sim_matrix] Error: no support for multiple fastas in leaves (triggered by {} in leaf {})".format(path, leaf.name)
-        assigned_fns.append(os.path.join(lib_dir, path))
+
+        complete_paths = [os.path.join(lib_dir, '.'.join(p.split('.')[:-1])) for p in path.split('@')]
+        for i, cp in enumerate(complete_paths):
+            if os.path.isfile("{}.kraken".format(cp)):
+                complete_paths[i] = "{}.kraken".format(cp)
+            elif os.path.isfile("{}.sam".format(cp)):
+                complete_paths[i] = "{}.sam".format(cp)
+            elif os.path.isfile("{}.bam".format(cp)):
+                complete_paths[i] = "{}.bam".format(cp)
+            else:
+                print('[prophyle_sim_matrix] Warnig: reference genome {} has no assignment file associated (please use the same prefix as the reference, and .kraken, .sam or .bam suffix depending on the classification format)'.format(cp), file=sys.stderr)
+                complete_paths[i] = None
+        assigned_fns.append(list(complete_paths))
+
+    sim_matrix = np.array((len(vec_pos), len(vec_pos)))
+    completed = 0
 
     with concurrent.futures.ThreadPoolExecutor(max_workers=jobs) as executor:
         # Start the load operations and mark each future with its URL
-        future_to_simcolumn = {executor.submit(analyse_assignments, ass_fn, nodes2nodes2leaves, vec_pos): ass_fn for ass_fn in assigned_fns}
-        for future in concurrent.futures.as_completed(future_to_url):
-            url = future_to_url[future]
+        future2simcolumn = {executor.submit(analyse_assignments, ass_fn, nodes2leaves, vec_pos): i for i, ass_fn in enumerate(assigned_fns)}
+        for future in concurrent.futures.as_completed(future2simcolumn):
+            i = future2simcolumn[future]
             try:
-                data = future.result()
-            except Exception as exc:
-                print('%r generated an exception: %s' % (url, exc))
-            else:
-                print('%r page is %d bytes' % (url, len(data)))
+                completed += 1
+                column_i = future.result()
+            except Exception as e:
+                print('[prophyle_sim_matrix] Warning: leaf {} generated an exception: {}. Setting its column to 0 0 .. 1[i] .. 0 0'.format(i, e), file=sys.stderr)
+                column_i = np.zeros(len(vec_pos))
+                column_i[i] = 1
+            print('[prophyle_sim_matrix] {}%% Leaf {} acquired'.format(completed/len(vec_pos), i))
+            sim_matrix[:,i] = column_i
+
+    np.save(out_fn, sim_matrix)
 
 def parse_args():
     parser = argparse.ArgumentParser(description='Classify reads simulated from the reference genomes to their index, in order to compute a similarity matrix for abundance estimation')