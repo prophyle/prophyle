77813adbd31e090fb58efa59f17b98a07dd63149
Simone Pignotti <pignottisimone@gmail.com>
2018-08-17 17:27:07 -0400

    Filter out genomes with no assignments to increase numerical stability

--- prophyle/prophyle_profile.py
+++ prophyle/prophyle_profile.py
@@ -108,6 +108,10 @@ def estimate_abundances(tree_fn, asg_fn, sim_mat_fn, out_fn, rcount_thresh=10, a
     sim_mat = np.load(sim_mat_fn)
     assert len(leaves) == len(sim_mat), "Size of similarity matrix different from #leaves...have you used the right index/tree?"
 
+    select_idx = map_counts > 0
+    map_counts = map_counts[select_idx]
+    sim_mat = sim_mat[np.ix_(select_idx, select_idx)]
+
     enet = ElasticNet(
         alpha=alpha,
         l1_ratio=l1_ratio,
@@ -124,13 +128,6 @@ def estimate_abundances(tree_fn, asg_fn, sim_mat_fn, out_fn, rcount_thresh=10, a
 
     if enet.score(sim_mat, map_counts) < 0.8:
         print("[prophyle_abundances] Warning: the fit to the model is quite poor, you may want to lower the alpha regularization parameter", file=sys.stderr)
-
-    abund = enet.coef_/sum(enet.coef_)
-    select_idx = abund > 1e-4
-    map_counts = map_counts[select_idx]
-    assert all(map_counts > 0)
-    sim_mat = sim_mat[np.ix_(select_idx, select_idx)]
-    map_counts = np.log(map_counts)
 
     enet.fit(sim_mat, np.log(map_counts))
     abund = enet.coef_/sum(enet.coef_)
